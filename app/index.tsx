c0f19e25e847f0cc78038180405161be6f9de66b
neco <ng.dinhduc.2311@gmail.com>
2025-07-07 06:29:26 +0000

    add Tailwind CSS and NativeWind integration with layout and styling updates

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
+import "./globals.css"
 import { Text, View } from "react-native";
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+export default function App() {
+    return (
+        <View className="flex-1 items-center justify-center bg-white">
+            <Text className="text-5xl text-dark-200 font-bold ">
+                Welcome!
+            </Text>
+        </View>
+    );
 }
